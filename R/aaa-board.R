25282b2e05b8b7f568e8ffac0f5ca7dc6bc547fb
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-03-01 16:41:50 -0800

    update modeldatatoo_board

--- R/aaa-board.R
+++ R/aaa-board.R
@@ -1,22 +1,6 @@
 github_raw <- function(x) paste0("https://raw.githubusercontent.com/", x)
 
-library(pins)
-
-## with a named vector of URLs to specific pins:
-b1 <- board_url(c(
-  files = github_raw("rstudio/pins-r/main/tests/testthat/pin-files/"),
-  rds = github_raw("rstudio/pins-r/main/tests/testthat/pin-rds/"),
-  raw = github_raw("rstudio/pins-r/main/tests/testthat/pin-files/first.txt")
+modeldatatoo_board <- pins::board_url(c(
+  ames = github_raw("emilhvitfeldt/modeldatatoo/main/data-raw/ames/20230302T001820Z-c271b/"),
+  elevators = github_raw("emilhvitfeldt/modeldatatoo/main/data-raw/elevators/20230302T002412Z-8e16a/")
 ))
-
-b1 %>% pin_read("rds")
-
-b1 %>%
-  pin_download("raw")
-
-b1 <- board_url(c(
-  ames = github_raw("emilhvitfeldt/modeldatatoo/main/data-raw/ames.rds")
-))
-
-b1 %>% pin_read("ames")
-
