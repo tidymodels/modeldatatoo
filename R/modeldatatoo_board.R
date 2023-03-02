454c94f3d51da093c22217bdce252daaf2b49086
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-03-01 16:58:27 -0800

    add internal_board() function

---
+++ R/modeldatatoo_board.R
@@ -0,0 +1,14 @@
+#' Internal pins board
+#'
+#' Pins board used internally to manage download, reading, and caching of data
+#' sets.
+#'
+#' @return a pins board
+#'
+#' @examples
+#' internal_board()
+#'
+#' @export
+internal_board <- function() {
+  modeldatatoo_board
+}
