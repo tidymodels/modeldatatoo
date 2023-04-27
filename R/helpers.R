12c5d78a157f02d7253b8f7dc59de497077d44c3
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-04-26 17:08:05 -0700

    add helper function to help add collections of data sets

--- R/helpers.R
+++ R/helpers.R
@@ -14,16 +14,78 @@
   return(invisible())
 }
 
+.add_new_data_collection <- function(name = NULL, data_sets = NULL) {
+  if (is.null(name)) {
+    stop("Please supply file name to `name`.")
+  }
+
+  if (is.null(data_sets)) {
+    stop("Please supply all object names name to `data_sets`.")
+  }
+
+  # R file
+  file_r <- paste0("R/", name, ".R")
+
+  if (file.exists(file_r)) {
+    stop("`name` already used, please use another.")
+  }
+
+  lines <- readLines("inst/template-r-file-collection")
+  lines <- gsub("NAME_OF_DATA_SET", name, lines)
+  lines <- gsub("OTHER_ALIASES", paste(data_sets, collapse = " "), lines)
+
+  TIBBLE_PRINTS_index <- which(lines == "TIBBLE_PRINTS")
+
+  TIBBLE_PRINTS_insert <- c(
+    paste0("#' load_", name ,"()"),
+    "#'",
+    paste0("#' ", data_sets)
+  )
+
+  lines <- c(
+    lines[seq(1, TIBBLE_PRINTS_index - 1)],
+    TIBBLE_PRINTS_insert,
+    lines[seq(TIBBLE_PRINTS_index + 1, length(lines))]
+  )
+
+  GLIMPSE_PRINTS_index <- which(lines == "GLIMPSE_PRINTS")
+
+  GLIMPSE_PRINTS_insert <- c(
+    paste0("#' tibble::glimpse(", data_sets, ")")
+  )
+
+  lines <- c(
+    lines[seq(1, GLIMPSE_PRINTS_index - 1)],
+    GLIMPSE_PRINTS_insert,
+    lines[seq(GLIMPSE_PRINTS_index + 1, length(lines))]
+  )
+
+  file.create(file_r)
+  writeLines(lines, file_r)
+  if (rstudioapi::isAvailable() && rstudioapi::hasFun("navigateToFile")) {
+    rstudioapi::navigateToFile(file_r)
+  }
+  else {
+    utils::file.edit(file_r)
+  }
+
+
+  # data-raw file
+  file_data_raw <- paste0("data-raw/", name, ".R")
+  setup_file(file_data_raw, name, "inst/template-data-raw-collection")
+
+  return(invisible())
+}
+
 setup_file <- function(file, name, template) {
   if (file.exists(file)) {
     stop("`name` already used, please use another.")
   }
 
+  lines <- readLines(template)
+  lines <- gsub("NAME_OF_DATA_SET", name, lines)
   file.create(file)
-
-  lines_data_raw <- readLines(template)
-  lines_data_raw <- gsub("NAME_OF_DATA_SET", name, lines_data_raw)
-  writeLines(lines_data_raw, file)
+  writeLines(lines, file)
   if (rstudioapi::isAvailable() && rstudioapi::hasFun("navigateToFile")) {
     rstudioapi::navigateToFile(file)
   }
