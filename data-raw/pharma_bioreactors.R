c6b931adfca8af4a179448364a3b3ea585cd7de1
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2023-07-24 12:11:05 -0700

    use `load()` correctly

--- data-raw/pharma_bioreactors.R
+++ data-raw/pharma_bioreactors.R
@@ -1,7 +1,7 @@
 library(pins)
 library(here)
 
-pharma_bioreactors <- load("data-raw/pharma_bioreactors/pharma_bioreactors.RData")
+load("data-raw/pharma_bioreactors/pharma_bioreactors.RData")
 
 board <- board_folder(here("pkgdown/assets/pins-board"), versioned = FALSE)
 board |> pin_write(pharma_bioreactors, "pharma_bioreactors", type = "rds")
