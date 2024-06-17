84cae382e6e9759a40f27cc7ddfcb75e4ee06e03
Hannah Frick <hannah@posit.co>
2024-06-17 19:14:06 +0100

    add shelter cats

---
+++ R/cat_adoption.R
@@ -0,0 +1,58 @@
+#' Cat Adoption
+#'
+#' @description
+#' A subset of the cats at the animal shelter in Long Beach, California, USA.
+#'
+#' @param ... Arguments passed to [pins::pin_read()].
+#' @return tibble
+#'
+#' @details
+#'
+#' A data frame with 2257 rows and 18 columns:
+#' \describe{
+#'   \item{event_time}{The time the cat spent at the shelter. The event is 
+#' adoption or the cat being returned to the place they belong. The non-event is
+#' the cat being transferred to another shelter or dying.}
+#'   \item{sex}{The sex of the cat and whether they are spayed or neutered.}
+#'   \item{intake_condition}{The intake condition of the cat.}
+#'   \item{intake_type}{The type of intake.}
+#'   \item{latitude}{Latitude of the intersection/cross street of intake or capture.}
+#'   \item{longitude}{Longitude of the intersection/cross street of intake or capture.}
+#'   \item{black}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{brown}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{brown_tabby}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{calico}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{cream}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{gray}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{gray_tabby}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{orange}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{orange_tabby}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{tan}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{tortie}{Indicator for the color/pattern of the cat's fur.}
+#'   \item{white}{Indicator for the color/pattern of the cat's fur.}
+#' }
+#'
+#' # tibble print
+#'
+#' ```{r}
+#' data_cat_adoption()
+#' ```
+#'
+#' # glimpse()
+#'
+#' ```{r}
+#' tibble::glimpse(data_cat_adoption())
+#' ```
+#'
+#' @source
+#' 
+#' <https://data.longbeach.gov/explore/dataset/animal-shelter-intakes-and-outcomes/information/>
+#'
+#' @examples
+#' \donttest{
+#' data_cat_adoption()
+#' }
+#' @export
+data_cat_adoption <- function(...) {
+  pins::pin_read(modeldatatoo_board(), "cat_adoption", ...)
+}
---
+++ data-raw/cat_adoption.R
@@ -0,0 +1,118 @@
+## code to prepare `cat_adoption` dataset goes here
+
+library(tidyverse)
+library(janitor)
+library(recipes)
+library(survival)
+
+# data from
+# https://data.longbeach.gov/explore/dataset/animal-shelter-intakes-and-outcomes/information/
+
+harmonize_colors <- function(x) {
+  x <- gsub("mut", "", x)
+
+  x <- gsub("pt", "point", x)
+  x <- gsub("(brn)|(br )", "brown ", x)
+  x <- gsub("dil", "dilute", x)
+  x <- gsub("org", "orange", x)
+  x <- gsub("rd", "red", x)
+  x <- gsub("slvr", "silver", x)
+  x <- gsub("(crm )|(cr )", "cream ", x)
+  x <- gsub("(slvr)|(sl)", "silver", x)
+  x <- gsub("choc ", "chocolate ", x)
+  x <- gsub("(lc )|(li )", "lilac ", x)
+  x <- gsub("l-c", "lilac_cream", x, fixed = TRUE)
+  x <- gsub("(bl )", "blue ", x)
+  x <- gsub("^(y )", "yellow ", x)
+  x <- gsub("(blk)|(bc)|(bk)", "black", x)
+
+  # fur patterns
+  x <- gsub("brind$", "brindle", x)
+  x <- gsub("tab$", "tabby", x)
+
+  # Things that are still unclear
+  x <- gsub("b-c", "", x, fixed = TRUE) # "brown-cream"?"
+  x <- gsub("s-t", "", x, fixed = TRUE)
+  
+  x <- trimws(x, which = "both")
+  gsub("[[:space:]]+", "_", x)
+}
+
+raw <- read_csv("data-raw/animal-shelter-intakes-and-outcomes.csv") %>% 
+  clean_names() %>% 
+  filter(animal_type == "CAT" & 
+          !is.na(outcome_type) &  
+          intake_is_dead == "Alive on Intake" & 
+          primary_color != "UNKNOWN" &
+          secondary_color != "UNKNOWN") %>% 
+  filter(
+    !(outcome_type %in% c("DISPOSAL", "EUTHANASIA", "MISSING", "DUPLICATE")) 
+  ) %>% 
+  # There are multiple rows for some animals; take most recent
+  arrange(animal_id, outcome_date) %>%
+  slice_head(by = c(animal_id), n = 1)
+
+event_list <- 
+  c("adoption", "community cat", "foster", "foster to adopt", "homefirst", 
+    "return to owner", "return to wild habitat", "shelter, neuter, return", 
+    "trap, neuter, release")
+
+other_list <- 
+  c("died", "rescue", "return to rescue", "transfer", "transport")
+
+cats <- raw %>% 
+  mutate(
+    across(where(is.character), tolower),
+    time = as.numeric(difftime(outcome_date, intake_date, units = "days")),
+    time = if_else(time < 0, NA_real_, time),
+    time = if_else(time < 1, 1, time),
+    event = if_else(outcome_type %in% event_list, 1, 0),
+  ) %>% 
+  filter(outcome_type %in% c(event_list, other_list) & time > 7) %>% 
+  select(time, event, contains("color"), sex, intake_condition, intake_type, 
+         jurisdiction, latitude, longitude, animal_id) %>% 
+  mutate(
+    # clean up color labels
+    primary_color = harmonize_colors(primary_color),
+    secondary_color = harmonize_colors(secondary_color),
+    # underscores
+    intake_condition = gsub("age/weight", "age_or_weight", intake_condition),
+    intake_condition = gsub("[[:space:]]+", "_", intake_condition),
+    intake_type = gsub("i/i", "i_i", intake_type, fixed = TRUE),
+    intake_type = gsub("[[:punct:]]", "", intake_type),
+    intake_type = gsub("[[:space:]]+", "_", intake_type),
+    jurisdiction = gsub("[[:space:]]+", "_", jurisdiction)
+  )
+
+# Make indicators for color (which also contains pattern)
+col_names <- function(var, lvl, ...) {
+  lvl
+}
+cats_with_color_dummies <- cats %>% 
+  recipe() %>% 
+  step_dummy_multi_choice(ends_with("color"), threshold = 0.0, naming = col_names) %>% 
+  step_other(intake_condition, intake_type, threshold = 0.02) %>%
+  step_zv() %>% 
+  prep() %>% 
+  bake(new_data = NULL)
+
+col_counts <- map_int(cats_with_color_dummies %>% select(-(1:9)), sum)
+col_count_rm <- names(col_counts)[col_counts <= 20]  
+
+cat_adoption <- 
+  cats_with_color_dummies %>% 
+  select(-all_of(col_count_rm)) %>% 
+  mutate(event_time = Surv(time, event)) %>% 
+  select(-time, -event, -animal_id, -jurisdiction) %>% 
+  relocate(event_time)
+
+
+
+# write to pin -----------------------------------------------------------
+
+# library(pins)
+# library(here)
+
+# board <- board_folder(here("pkgdown/assets/pins-board"), versioned = FALSE)
+# board |> pin_write(cat_adoption, "cat_adoption", type = "rds")
+# board |> write_board_manifest()
